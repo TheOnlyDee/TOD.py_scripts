492d302e15bff908427256ba61c550b9865ff5e8
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-10-09 10:04:06 +0200

    Add files via upload

---
+++ lessionone.py
@@ -0,0 +1,18 @@
+
+#
+#print ("Hello World!!!!!")
+#print ('I am iron man')
+#print ("NO, I am Tony Stark")
+#print ("No, I am Poppy")
+
+
+#We are praticing python...... IT'S AWESOME!!!	
+
+#print ("""I am Iron Man.
+#No, I am Tony Stark
+#No , I am Poppy""")
+
+#print ("I am Iron Man." + " No, I am Tony Stark." + " No, I am Poppy.")
+
+print (" I am Poppy\n " * 100 )
+
