d84992bc740e794aec19772da0da1ea2c472db02
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-10-09 10:06:06 +0200

    Add files via upload

---
+++ lessionthree.py
@@ -0,0 +1,22 @@
+#math is cool
+
+name = "Paolo"
+
+age = 13
+
+actual_age = 13.6
+
+#print ( name)
+
+#print ( age )
+
+#print ( type ( name ))
+
+#print ( type ( age ))
+
+#print ( type ( actual_age ))
+
+math = 5 ** 7 + 6 / 9 * 6 - 4
+
+results = age + actual_age + math
+print ( results )
