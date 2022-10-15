af2860c02b220b92aaf1238bdf25ea634cd9c963
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-10-15 07:52:25 +0000

    SELETTORE DI TEAM

---
+++ teamselector.py
@@ -0,0 +1,12 @@
+#primo progetto python del libro "CODING: Guida Facile Per Principianti" (apogeonline.com)
+#messo in pratica da TOD (https://github.com/TheOnlyDee)
+
+import random
+
+import time
+
+time.sleep(1)
+
+print("Benvenuto su Team Allocator!!")
+
+players = ["Martin", "Craig", "Sue", "Claire"]
